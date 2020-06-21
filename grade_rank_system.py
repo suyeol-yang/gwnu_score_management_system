c8e1669c68050c9ef685412fccce50941b166794
Yang soo yeol <suyeal123@naver.com>
2020-06-21 17:44:56 +0900

    2일차 GradeRankSystem write 메소드 구현 rank 최종 수정

--- grade_rank_system.py
+++ grade_rank_system.py
@@ -22,8 +22,9 @@ class GradeRankSystem:
 
         return len(self._grades)
 
-    def _make_grades_string(self,order_key,grades):
-        rank = 1;
+    def _make_grades_string(self,order_key,order_way,grades):
+        rank_asc = 1
+        rank_des = int(len(self._grades))
         result = ""
         for key, item in grades:
             result = result + item.gid + ","
@@ -36,8 +37,12 @@ class GradeRankSystem:
             if order_key == 'rank':
                 result = result.rstrip("\n")
                 result = result + ","
-                result = result + str(rank) + "\n"
-            rank = rank + 1
+                if order_way == 'asc':
+                    result = result + str(rank_asc) + "\n"
+                elif order_way == 'des':
+                    result = result + str(rank_des) + "\n"
+            rank_asc = rank_asc + 1
+            rank_des = rank_des - 1
         return result.strip()
 
     def sort(self,order_key="rank", order_way="asc"):
@@ -50,6 +55,12 @@ class GradeRankSystem:
         elif order_key == "gid" and order_way == "des":
             sorted_grades = sorted(self._grades.items(),reverse=True)
 
-        result = self._make_grades_string(order_key, sorted_grades)
+        result = self._make_grades_string(order_key, order_way, sorted_grades)
         return result
+
+
+    def write(self,file_name,order_key="rank", order_way="asc"):
+        with open(file_name, 'wt', encoding="utf-8") as fo:
+            result = self.sort(order_key,order_way)
+            fo.write(result)
 
--- test_grade_rank_system.py
+++ test_grade_rank_system.py
@@ -10,7 +10,11 @@ class TestGradeRankSystem(unittest.TestCase):
     def setUp(self):
         self.m_open_1 = mock_open(read_data = "1,양수열,95,92,88\n")
         self.m_open_2 = mock_open(read_data = "1,양수열,95,92,88\n2,이승수,80,70,60\n\n")
-        self.m_open_3 = mock_open(read_data = "1,양수열,95,92,88\n2,이승수,80,70,60\n3,장규범,75,85,80\n")
+
+        self.m_write_open_1 = mock_open()
+        self.m_w = mock_open().return_value
+        self.m_write_open_1.side_effect = [self.m_open_2.return_value,self.m_w]
+
 
     def test_constructor(self):
         tgrs = GradeRankSystem()
@@ -31,11 +35,6 @@ class TestGradeRankSystem(unittest.TestCase):
             self.assertEqual(2,tgrs.read('grade_data1.csv'))
 
 
-    def test_read_3(self):
-
-        with patch('grade_rank_system.open',self.m_open_3):
-            tgrs = GradeRankSystem()
-            self.assertEqual(3,tgrs.read('grade_data1.csv'))
 
     def test_sort_1(self):
         with patch('grade_rank_system.open',self.m_open_1):
@@ -78,6 +77,38 @@ class TestGradeRankSystem(unittest.TestCase):
         result = tgrs.sort(order_way="des")
         self.assertEqual('2,이승수,80,70,60,210,70.0,2\n1,양수열,95,92,88,275,91.7,1',result)
 
+
+    def test_write_1(self):
+        with patch('grade_rank_system.open',self.m_write_open_1):
+            tgrs = GradeRankSystem()
+            tgrs.read('grade.csv')
+            tgrs.write('result.csv')
+
+        self.m_w.write.assert_called_with('1,양수열,95,92,88,275,91.7,1\n2,이승수,80,70,60,210,70.0,2')
+
+    def test_write_2(self):
+        with patch('grade_rank_system.open',self.m_write_open_1):
+            tgrs = GradeRankSystem()
+            tgrs.read('grade.csv')
+            tgrs.write('result.csv','rank','des')
+
+        self.m_w.write.assert_called_with('2,이승수,80,70,60,210,70.0,2\n1,양수열,95,92,88,275,91.7,1')
+
+    def test_write_3(self):
+        with patch('grade_rank_system.open',self.m_write_open_1):
+            tgrs = GradeRankSystem()
+            tgrs.read('grade.csv')
+            tgrs.write('result.csv','gid','asc')
+
+        self.m_w.write.assert_called_with('1,양수열,95,92,88,275,91.7\n2,이승수,80,70,60,210,70.0')
+
+    def test_write_4(self):
+        with patch('grade_rank_system.open',self.m_write_open_1):
+            tgrs = GradeRankSystem()
+            tgrs.read('grade.csv')
+            tgrs.write('result.csv','gid','des')
+
+        self.m_w.write.assert_called_with('2,이승수,80,70,60,210,70.0\n1,양수열,95,92,88,275,91.7')
 
 if __name__ == "__main__":
     unittest.main()
